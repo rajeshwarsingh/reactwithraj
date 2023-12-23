155bc89dba5ac499ff2b3a67a0b2259f1c2a4a1f
tidbit <thetidbitcompany@gmail.com>
2023-12-23 17:54:17 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+// <div id='parent'>
+//   <div id='child'>
+//     <h1 id='heading1'>i am heading</h1>
+//   </div>
+// </div>
+
+const parent = React.createElement('div', { id: 'parent' },
+  [React.createElement('div', { id: 'child1' }, 
+  [React.createElement('h1', {}, 'i am heading one'),
+  React.createElement('h2', {}, 'i am heading two')]
+  ),
+  React.createElement('div', { id: 'child2' }, 
+  [React.createElement('h1', {}, 'i am heading one'),
+  React.createElement('h2', {}, 'i am heading two')]
+  )]
+  );
+
+  console.log(parent)
+
+const heading = React.createElement('h1', { id: 'heading' }, 'testing hello world in react js');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
